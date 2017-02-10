581f8179c24ac991b6c07eef3967f0e2107baad2
dmpayton <derek.payton@gmail.com>
2017-02-09 16:27:42 -0800

    More admin stuff.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,7 +4,9 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
     list_display = ['title', 'created_at', 'status', 'author']
+    list_filter = ['status', 'created_at', 'author']
 
 
 admin.site.register(Post, PostAdmin)
